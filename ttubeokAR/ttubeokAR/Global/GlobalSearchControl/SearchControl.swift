136338ccf4ef0d9c5f55b4548cb2811d563b2c6c
JEONG-J <ikejhc159@gmail.com>
2024-01-09 21:50:06 +0900

    Feat: 폰트 extension 및 검색바 맵뷰 메인뷰에 따른 간편 검색 버튼 변경 기능
    검색바 밑 간편 검색 버튼을 현재 화면에 맞추어 변경되도록 변경
    애플 산돌고딕 폰트를 쉽게 사용하도록 extension 만듦

--- ttubeokAR/ttubeokAR/Global/GlobalSearchControl/SearchControl.swift
+++ ttubeokAR/ttubeokAR/Global/GlobalSearchControl/SearchControl.swift
@@ -18,7 +18,7 @@ struct SearchControl: View {
         ZStack{
             Color(red: 0.09, green: 0.08, blue: 0.12).ignoresSafeArea()
             VStack{
-                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 12) {
+                HStack(alignment: .center, spacing: 12) {
                     
                     ZStack{
                         Rectangle()
@@ -27,7 +27,7 @@ struct SearchControl: View {
                             .clipShape(.rect(cornerRadius: 12))
                         Image("logo")
                             .resizable()
-                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .aspectRatio(contentMode: .fit)
                             .frame(width: 43, height: 31)
                     }// 뚜벅 로고
                     
@@ -36,8 +36,12 @@ struct SearchControl: View {
                             .resizable()
                             .frame(width: 24, height: 24)
                             .padding(8)
+                        //MARK: - TODO
+                        /**
+                         - Note : 텍스트필드 내 기본 텍스트 SWiftUI에서 색상 변경 불가
+                         */
                         TextField("길 검색하기", text: $viewModel.searchText)
-                            .font(.system(size: 14))
+                            .font(.sandol(type: .regular, size: 14))
                             .foregroundStyle(Color(red: 0.52, green: 0.53,  blue: 0.6))
                             .frame(minWidth: 0, maxWidth: 239)
                         
@@ -71,28 +75,57 @@ struct SearchControl: View {
                 .padding(.vertical, 2)
                 
                 //MARK: - TODO
-                /*
+                /**
+                 
                  - Note: 지도뷰 / 메인 뷰에 따라 하단 간편 검색 기록의 종류가 바뀌어야 한다. 추후 지도를 제작 후 다시 모델을 수정할 예정!!
-                 */
+                 
+                 **/
                 GeometryReader { geometry in
                     HStack(alignment: .center, spacing: 4) {
-                        ForEach(viewModel.buttons.indices, id: \.self) { index in
-                            Button(viewModel.buttons[index].title) {
-                                viewModel.buttonSelection(at: index)
-                                viewModel.buttons[index].action()
+                        ScrollView(.horizontal, showsIndicators: false){
+                            HStack(alignment: .center, spacing: 4){
+                                ForEach(viewModel.buttons.indices, id: \.self) { index in
+                                    Button(action: {
+                                        viewModel.buttonSelection(at: index)
+                                        viewModel.buttons[index].action()
+                                    })
+                                    {
+                                        HStack {
+                                            if !viewModel.buttons[index].buttonImage.isEmpty {
+                                                Image(viewModel.buttons[index].buttonImage)
+                                                    .resizable()
+                                                    .frame(width: 20, height: 20)
+                                            }
+                                            Text(viewModel.buttons[index].title)
+                                                .font(.sandol(type: .regular, size: 12))
+                                                .foregroundStyle(Color.white)
+                                        }
+                                        .frame(width: geometry.size.width > 500 ? 71 : geometry.size.width / 5, height: 32)
+                                        .background(viewModel.selectedButtonIndex == index ? Color(red: 0.52, green: 0.54, blue: 0.92) : Color(red: 0.16, green: 0.16, blue: 0.23))
+                                        .foregroundStyle(Color(red: 0.92, green: 0.9, blue: 0.97))
+                                        .clipShape(.rect(cornerRadius: 16))
+                                    }
+                                }
                             }
-                            .font(.system(size: 12))
-                            .frame(width: geometry.size.width > 500 ? 71 : geometry.size.width / 5, height: 32)
-                            .background(viewModel.selectedButtonIndex == index ? Color(red: 0.52, green: 0.54, blue: 0.92) : Color(red: 0.16, green: 0.16, blue: 0.23))
-                            .foregroundStyle(Color(red: 0.92, green: 0.9, blue: 0.97))
-                            .clipShape(.rect(cornerRadius: 16))
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 0)// 하단 간편 검색 버튼 생성
                         }
+                        .frame(width: geometry.size.width)
                     }
-                    .frame(width: 324, alignment: .topLeading)
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 0)
-                } // 하단 간편 검색 버튼 생성
+                    .frame(width: 60, alignment: .topLeading)
+                }
+                .frame(maxWidth: .infinity)
             }
         }
+    }
+}
+
+struct SearchControl_Previews: PreviewProvider {
+    static var previews: some View {
+        // Create an instance of SearchViewModel
+        let viewModel = SearchViewModel()
+        
+        // Pass the viewModel to the test1 view
+        SearchControl(viewModel: viewModel)
     }
 }
