8b5bfc83c3e843dcecc83286a4376f635580d108
realhsb <realhsb@gmail.com>
2024-01-21 02:39:41 +0900

    Feat: 설정 - 등록된 장소 관리 탭 추가
    설정 - 등록된 장소 관리 탭 추가. 디자인 수정 필요

---
+++ ttubeokAR/ttubeokAR/SettingView/SettingModel/Manage/Place.swift
@@ -0,0 +1,14 @@
+//
+//  Place.swift
+//  ttubeokAR
+//
+//  Created by Subeen on 1/21/24.
+//
+
+import Foundation
+
+struct Place: Hashable {
+    var image: String
+    var title: String
+    var content: String
+}
