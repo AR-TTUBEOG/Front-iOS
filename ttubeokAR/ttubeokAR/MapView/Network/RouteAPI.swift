f6d5015ea78bb35a078d8af5594892f6d07b65c2
realhsb <realhsb@gmail.com>
2024-02-07 15:17:05 +0900

    Feat: OSRM API 연결
    출발지/도착지 위도/경로 입력시 최단경로를 구해주는 OSRM API 연결

---
+++ ttubeokAR/ttubeokAR/MapView/Network/RouteAPI.swift
@@ -0,0 +1,17 @@
+//
+//  RouteAPI.swift
+//  ttubeokAR
+//
+//  Created by Subeen on 1/23/24.
+//
+
+import Foundation
+
+class RouteAPI {
+    static let baseUrl: String = "http://router.project-osrm.org/"
+//    static let baseUrl: String = "http://127.0.0.1:5001/"
+//    static let secondUrl: String = "route/v1/foot"
+    static let secondUrl: String = "route/v1/driving"
+    
+// http://127.0.0.1:5001/route/v1/foot/127.001160,37.267324;127.007037,37.269611?steps=true&geometries=geojson
+}
